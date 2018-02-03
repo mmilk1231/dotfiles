78df35be52f3e6a118ce3fd375e2aba19c15e511
Megumi Miyashita <penguin.9922@gmail.com>
2018-02-04 03:28:02 +0900

    Add good Python environment

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,21 +4,11 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-;; 言語を日本語にする
-(set-language-environment 'Japanese)
-;; 極力UTF-8とする
-(prefer-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-;; shellの文字化けを回避
-(add-hook 'shell-mode-hook(lambda ()(set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix)))
-;; メニューバーを表示しない
-(menu-bar-mode -1)
-;; OS判定
+;; Decision OS
 (setq sysname system-type)
 
 (if (eq sysname 'cygwin)
-    ;; クリップボードの共有
+    ;; Share the clipboard
     (progn
       (defun paste-from-cygwin ()
 	(with-temp-buffer
@@ -29,14 +19,14 @@
 	  (insert text)))
       (setq interprogram-cut-function 'cut-to-cygwin)
       (setq interprogram-paste-function 'paste-from-cygwin)
-      ;; caskの設定
+      ;; Set cask
       (require 'cask "~/.cask/cask.el")
       (cask-initialize)
       )
   (message "This platform is not cygwin")
   )
 (if (eq sysname 'darwin)
-    ;; クリップボードの共有
+    ;; Share the clipboard
     (progn
       (defun copy-from-osx ()
 	(shell-command-to-string "pbpaste"))
@@ -47,12 +37,37 @@
 	    (process-send-eof proc))))
       (setq interprogram-cut-function 'paste-to-osx)
       (setq interprogram-paste-function 'copy-from-osx)
-      ;; caskの設定
+      ;; Set cask
       (require 'cask)
       (cask-initialize)
       )
   (message "This platform is not mac")
   )
 
+;; Company mode
+(use-package company
+  :init (add-hook 'after-init-hook 'global-company-mode))
+;; Set the language to Japanese
+(set-language-environment 'Japanese)
+;; Use utf-8
+(prefer-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+;; Avoid text garbling in the shell
+(add-hook 'shell-mode-hook(lambda ()(set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix)))
+;; Do not show menubar
+(menu-bar-mode -1)
+;; Python
+(use-package jedi-core
+  :config (setq jedi:complete-on-dot t)
+          (setq jedi:use-shortcuts t)
+          (add-hook 'python-mode-hook 'jedi:setup)
+          (add-to-list 'company-backends 'company-jedi))
+(use-package py-autopep8
+  :config (add-hook 'python-mode-hook 'py-autopep8-enable-on-save))
+(use-package flymake-python-pyflakes
+  :config (flymake-mode t)
+          (add-hook 'python-mode-hook 'flymake-python-pyflakes-load))
+;; Nyan mode
 (use-package nyan-mode
   :init (nyan-mode 1))
--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # README #
 
-## requirements (WIP) ##
-- cask (for emacs): http://cask.readthedocs.io/en/latest/
+## Requirements (WIP) ##
+- cask (for Emacs): http://cask.readthedocs.io/en/latest/
 `brew install cask`
+
+- virtualenv (for Jedi)
+`pip install virtualenv`
+
+- pyflakes (for flymake-python-pyflakes)
+`pip install pyflakes`
+
+- autopep8 (for autopep8)
+`pip install autopep8`
+
+## Setting
+`cd <your cask path>`
+`cask install`
