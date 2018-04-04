e3d49aa1e73a43181bbbc354bb8c36f1be24b46b
Megumi Miyashita <penguin.9922@gmail.com>
2018-04-04 22:30:26 +0900

    Modify init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,7 +6,6 @@
 
 ;; Recognition OS
 (setq sysname system-type)
-
 (if (eq sysname 'cygwin)
     ;; Share the clipboard
     (progn
@@ -26,13 +25,13 @@
   (message "This platform is not cygwin")
   )
 (if (eq sysname 'darwin)
-    ;; Share the clipboard
+   ;; Share the clipboard
     (progn
       (defun copy-from-osx ()
-	(shell-command-to-string "pbpaste"))
+	(shell-command-to-string "reattach-to-user-namespace pbpaste"))
       (defun paste-to-osx (text &optional push)
 	(let ((process-connection-type nil))
-	  (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+	  (let ((proc (start-process "pbcopy" "*Messages*" "reattach-to-user-namespace" "pbcopy")))
 	    (process-send-string proc text)
 	    (process-send-eof proc))))
       (setq interprogram-cut-function 'paste-to-osx)
