0b775f3431724f6660d17604efcf9067e6b019f0
Megumi Miyashita <penguin.9922@gmail.com>
2018-08-13 04:01:00 +0900

    Add setup shell for mac

---
+++ setup_mac.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Set parameters
+readonly PYTHON_VER="3.7.0"
+readonly BOOST_VER="1.68.0"
+readonly QT_VER="5.10"
+readonly GOI_VER="1.56.1"
+readonly POPPLER_VER="0.67.0"
+
+# XCode
+xcode-select --install
+
+# Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew upgrade
+brew doctor
+
+# Anyenv
+git clone https://github.com/riywo/anyenv ~/.anyenv
+
+# Python (pyenv, pyenv-virtualenv)
+anyenv install pyenv
+git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.anyenv/envs/pyenv/plugins/pyenv-virtualenv
+source ~/.bash_profile
+env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install $PYTHON_VER
+pyenv global $PYTHON_VER
+
+# flycheck dependency
+pip install epc
+pip install flake8
+
+# Emacs with cask
+brew install emacs
+brew install cask
+brew install reattach-to-user-namespace
+cd ~/.emacs.d/
+cask install
+
+# Roboschool dependecy
+## Boost
+git clone --recursive https://github.com/boostorg/boost.git ~/Documents/boost
+cd ~/Documents/boost
+git checkout "boost-${BOOST_VER}"
+source ~/.bash_profile
+./bootstrap.sh --with-python="$(pyenv prefix)/bin/python3"
+./b2 install
+
+## Qt
+brew install qt
+
+# Roboschool
+pip install -e $ROBOSCHOOL_PATH
+
+# Pympress dependency
+## GObject Introspection
+brew install gtk+3
+brew install libffi
+git clone https://github.com/GNOME/gobject-introspection.git ~/Documents/gobject-introspection
+cd ~/Documents/gobject-introspection
+git checkout "$GOI_VER"
+./autogen.sh
+./configure --with-python="$(pyenv prefix)/bin/python3"
+make -j4
+make install
+
+## Pygobject
+pip install pygobject
+
+## Poppler
+brew install openjpeg gettext
+git clone https://anongit.freedesktop.org/git/poppler/poppler.git ~/Documents/poppler
+cd ~/Documents/poppler
+git checkout "poppler-${POPPLER_VER}"
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_MACOSX_RPATH=OFF ..
+make -j4
+make install
+
+# Pyimpress
+pip install pyimpress
