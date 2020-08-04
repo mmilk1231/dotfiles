e726c718d69a491576c74a2413f89b43b53ca4ca
Megumi Miyashita <penguin.9922@gmail.com>
2020-08-04 14:49:53 +0900

    Add pyenv-update

--- setup_mac.sh
+++ setup_mac.sh
@@ -30,10 +30,11 @@ source ~/.bash_profile
 mkdir -p $(anyenv root)/plugins
 git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
     
-# Python (pyenv, pyenv-virtualenv)
+# Python (pyenv, pyenv-update, pyenv-virtualenv)
 anyenv install pyenv
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.anyenv/envs/pyenv/plugins/pyenv-virtualenv
 source ~/.bash_profile
+git clone https://github.com/pyenv/pyenv-update.git $(pyenv root)/plugins/pyenv-update
 env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install $PYTHON_VER
 pyenv global $PYTHON_VER
 
